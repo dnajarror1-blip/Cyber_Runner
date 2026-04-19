0b4b45b17df7eaf5a8cd073f76c8e197e063d51e
Darwin <dnajarror1@miumg.edu.gt>
2026-04-19 15:19:42 -0600

    Juego funcional

--- include/core/Game.h
+++ include/core/Game.h
@@ -3,7 +3,6 @@
 //
 
 #pragma once
-#include "raylib.h"
 #include "entities/Player.h"
 #include "entities/Obstacle.h"
 #include <vector>
--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -24,7 +24,7 @@ void Game::resetGame() {
 
 void Game::run() {
 
-    InitWindow(screenWidth, screenHeight, "Cyber-Runner: Neo-Guate");
+    InitWindow(screenWidth, screenHeight, "Cyber-Runner: Neo");
     SetTargetFPS(60);
 
 
