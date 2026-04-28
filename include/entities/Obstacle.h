9f3234eb4c0f8a69084ee9f25a80170a416550a8
18JosePabloQuintoC <162483932+18JosePabloQuintoC@users.noreply.github.com>
2026-04-28 08:34:45 -0600

    Refactor Obstacle.h with header guards and access modifiers

--- include/entities/Obstacle.h
+++ include/entities/Obstacle.h
@@ -1,7 +1,9 @@
 //
 // Created by darwin on 18/04/26.
 //
-#pragma once
+#ifndef CYBER_RUNNER_OBSTACLE_H
+#define CYBER_RUNNER_OBSTACLE_H
+
 #include "raylib.h"
 
 class Obstacle {
@@ -9,17 +11,21 @@ private:
     Rectangle rect;
     float speed;
 
-    public:
+public:
+    // Constructor: Define posición, tamaño y velocidad inicial
     Obstacle(float x, float y, float width, float height, float speed);
 
+    // Mueve el obstáculo y lo recicla cuando sale de la pantalla
     void update();
+
+    // Dibuja el obstáculo en pantalla
     void draw();
 
+    // Permite cambiar la velocidad desde afuera (para la dificultad progresiva)
+    void setSpeed(float newSpeed);
+
+    // Devuelve el rectángulo para que el Integrante B pueda detectar colisiones
     Rectangle getRect();
-
 };
-
-#ifndef CYBER_RUNNER_OBSTACLE_H
-#define CYBER_RUNNER_OBSTACLE_H
 
 #endif //CYBER_RUNNER_OBSTACLE_H
