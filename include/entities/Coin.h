e84cdfc3c94fcb557a7d62d405dd7f67b7dcebe9
ajjac <jajcalonj1@miumg.edu.gt>
2026-05-16 10:55:15 -0600

    Robot implementation

--- include/entities/Coin.h
+++ include/entities/Coin.h
@@ -14,8 +14,27 @@ private:
     bool active;
     ItemType type;
 
+    Texture2D coinTexture1 {};
+    Texture2D coinTexture2 {};
+    Texture2D nitroTexture {};
+
+    Texture2D* currentTexture = nullptr;
+
+    float animationTimer = 0.0f;
+    float animationSpeed = 0.12f;
+
+    bool animationFrame = false;
+
 public:
     Coin(float x, float y, float size, float speed, ItemType type = ItemType::CREDIT);
+
+    ~Coin();
+
+    Coin(const Coin&) = delete;
+    Coin& operator=(const Coin&) = delete;
+
+    Coin(Coin&& other) noexcept;
+    Coin& operator=(Coin&& other) noexcept;
 
     void update(float deltaTime);
     void draw();
--- src/entities/Coin.cpp
+++ src/entities/Coin.cpp
@@ -3,14 +3,161 @@
 Coin::Coin(float x, float y, float size, float speed, ItemType type)
 {
     rect = {x, y, size, size};
+
     this->speed = speed;
     this->type = type;
+
     active = true;
+
+    coinTexture1 = LoadTexture("assets/coin1.png");
+    coinTexture2 = LoadTexture("assets/coin2.png");
+    nitroTexture = LoadTexture("assets/nitro.png");
+
+    currentTexture = &coinTexture1;
+}
+
+Coin::~Coin()
+{
+    if (coinTexture1.id)
+    {
+        UnloadTexture(coinTexture1);
+    }
+
+    if (coinTexture2.id)
+    {
+        UnloadTexture(coinTexture2);
+    }
+
+    if (nitroTexture.id)
+    {
+        UnloadTexture(nitroTexture);
+    }
+}
+
+Coin::Coin(Coin&& other) noexcept
+{
+    rect = other.rect;
+    speed = other.speed;
+    active = other.active;
+    type = other.type;
+
+    coinTexture1 = other.coinTexture1;
+    coinTexture2 = other.coinTexture2;
+    nitroTexture = other.nitroTexture;
+
+    animationTimer = other.animationTimer;
+    animationSpeed = other.animationSpeed;
+    animationFrame = other.animationFrame;
+
+    if (other.currentTexture == &other.coinTexture1)
+    {
+        currentTexture = &coinTexture1;
+    }
+    else if (other.currentTexture == &other.coinTexture2)
+    {
+        currentTexture = &coinTexture2;
+    }
+    else if (other.currentTexture == &other.nitroTexture)
+    {
+        currentTexture = &nitroTexture;
+    }
+    else
+    {
+        currentTexture = nullptr;
+    }
+
+    other.coinTexture1 = {};
+    other.coinTexture2 = {};
+    other.nitroTexture = {};
+    other.currentTexture = nullptr;
+}
+
+Coin& Coin::operator=(Coin&& other) noexcept
+{
+    if (this != &other)
+    {
+        if (coinTexture1.id)
+        {
+            UnloadTexture(coinTexture1);
+        }
+
+        if (coinTexture2.id)
+        {
+            UnloadTexture(coinTexture2);
+        }
+
+        if (nitroTexture.id)
+        {
+            UnloadTexture(nitroTexture);
+        }
+
+        rect = other.rect;
+        speed = other.speed;
+        active = other.active;
+        type = other.type;
+
+        coinTexture1 = other.coinTexture1;
+        coinTexture2 = other.coinTexture2;
+        nitroTexture = other.nitroTexture;
+
+        animationTimer = other.animationTimer;
+        animationSpeed = other.animationSpeed;
+        animationFrame = other.animationFrame;
+
+        if (other.currentTexture == &other.coinTexture1)
+        {
+            currentTexture = &coinTexture1;
+        }
+        else if (other.currentTexture == &other.coinTexture2)
+        {
+            currentTexture = &coinTexture2;
+        }
+        else if (other.currentTexture == &other.nitroTexture)
+        {
+            currentTexture = &nitroTexture;
+        }
+        else
+        {
+            currentTexture = nullptr;
+        }
+
+        other.coinTexture1 = {};
+        other.coinTexture2 = {};
+        other.nitroTexture = {};
+        other.currentTexture = nullptr;
+    }
+
+    return *this;
 }
 
 void Coin::update(float deltaTime)
 {
     rect.x -= speed * deltaTime;
+
+    animationTimer += deltaTime;
+
+    if (animationTimer >= animationSpeed)
+    {
+        animationTimer = 0.0f;
+
+        animationFrame = !animationFrame;
+    }
+
+    if (type == ItemType::CREDIT)
+    {
+        currentTexture =
+            animationFrame
+            ? &coinTexture1
+            : &coinTexture2;
+    }
+    else if (type == ItemType::NITRO)
+    {
+        currentTexture = &nitroTexture;
+    }
+    else
+    {
+        currentTexture = nullptr;
+    }
 
     if (rect.x + rect.width < 0)
     {
@@ -25,28 +172,51 @@ void Coin::draw()
         return;
     }
 
-    Color itemColor = GOLD;
-
-    if (type == ItemType::NITRO)
+    if (type == ItemType::SHIELD)
     {
-        itemColor = SKYBLUE;
+        DrawCircle(
+            rect.x + rect.width / 2,
+            rect.y + rect.height / 2,
+            rect.width / 2,
+            ORANGE
+        );
+
+        DrawCircleLines(
+            rect.x + rect.width / 2,
+            rect.y + rect.height / 2,
+            rect.width / 2,
+            WHITE
+        );
+
+        return;
     }
-    else if (type == ItemType::SHIELD)
+
+    if (currentTexture == nullptr || currentTexture->id == 0)
     {
-        itemColor = ORANGE;
+        DrawRectangleRec(rect, RED);
+        return;
     }
 
-    DrawCircle(
-        rect.x + rect.width / 2,
-        rect.y + rect.height / 2,
-        rect.width / 2,
-        itemColor
-    );
+    Rectangle source = {
+        0,
+        0,
+        (float)currentTexture->width,
+        (float)currentTexture->height
+    };
 
-    DrawCircleLines(
-        rect.x + rect.width / 2,
-        rect.y + rect.height / 2,
-        rect.width / 2,
+    Rectangle dest = {
+        rect.x,
+        rect.y,
+        rect.width,
+        rect.height
+    };
+
+    DrawTexturePro(
+        *currentTexture,
+        source,
+        dest,
+        {0,0},
+        0.0f,
         WHITE
     );
 }
@@ -55,7 +225,10 @@ void Coin::reset(float x)
 {
     rect.x = x;
     rect.y = GetRandomValue(180, 300);
+
     active = true;
+
+    animationTimer = 0.0f;
 
     int roll = GetRandomValue(0, 100);
 
@@ -99,6 +272,7 @@ bool Coin::isActive()
 void Coin::collect()
 {
     active = false;
+
     reset(800 + GetRandomValue(300, 800));
 }
 
